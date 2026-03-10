6af267959283131fb5419f3d6d7711134b40e4fb
armdupre <a-joseph.dupre@keysight.com>
2026-03-10 14:45:54 -0400

    Updated.

--- variables.tf
+++ variables.tf
@@ -43,13 +43,6 @@ variable "MachineType" {
 	default = "c2-standard-8"
 	description = "Designation for set of resources available to VM"
 	type = string
-	validation {
-		condition = contains([ "c2-standard-4", "c2-standard-8", "c2-standard-16" ], var.MachineType)
-		error_message = <<EOF
-MachineType must be one of the following types:
-	c2-standard-4, c2-standard-8, c2-standard-16
-		EOF
-	}
 }
 
 variable "MarketplaceImageName" {
